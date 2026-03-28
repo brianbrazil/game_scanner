576e81a1fe32d1cb611fa2939550feeb6955faef
brianbrazil <bbrazil@gmail.com>
2026-03-28 19:45:25 -0400

    Refactor title picker to extract button generation logic into separate methods

--- lib/title_selection_dialog.dart
+++ lib/title_selection_dialog.dart
@@ -17,45 +17,55 @@ class TitleSelectionDialog extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ...games.asMap().entries.map((entry) {
-              final index = entry.key;
-              final game = entry.value;
-              final isFirstGame = index == 0;
-              return SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    BggPage(
-                      gameInfo: game,
-                    ).open(context);
-                  },
-                  child: Center(
-                    child: Text(
-                      '${game.name} (${game.published})',
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.visible,
-                      style: TextStyle(fontSize: isFirstGame ? 22 : 14),
-                    ),
-                  ),
-                ),
-              );
-            }),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  side: BorderSide(color: Colors.red, width: 2),
-                ),
-                child: Text('Cancel'),
-              ),
-            ),
+            ...titleButtons(context),
+            cancelButton(context),
           ],
         ),
       ),
     );
+  }
+
+  Iterable<Widget> titleButtons(BuildContext context) {
+    return games.map((game) {
+      final isFirstGame = games.indexOf(game) == 0;
+      return titleButton(context, game, isFirstGame);
+    });
+  }
+
+  SizedBox titleButton(BuildContext context, GameInfo game, bool isFirstGame) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+          BggPage(
+            gameInfo: game,
+          ).open(context);
+        },
+        child: Center(
+          child: Text(
+            '${game.name} (${game.published})',
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.visible,
+            style: TextStyle(fontSize: isFirstGame ? 22 : 14),
+          ),
+        ),
+      ),
+    );
+  }
+
+  SizedBox cancelButton(BuildContext context) {
+    return SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                side: BorderSide(color: Colors.red, width: 2),
+              ),
+              child: Text('Cancel'),
+            ),
+          );
   }
 }

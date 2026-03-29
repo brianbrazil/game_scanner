07e9983aeffcab2a9acebfd89bfbc97e57ee97e3
brianbrazil <bbrazil@gmail.com>
2026-03-28 20:01:04 -0400

    Refactor TitleSelectionDialog to use ChangeNotifierProvider and simplify state management

--- lib/title_selection_dialog.dart
+++ lib/title_selection_dialog.dart
@@ -1,34 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'bgg_page.dart';
 import 'game_info.dart';
 
 class TitleSelectionDialog extends StatelessWidget {
-  const TitleSelectionDialog({
-    super.key,
-    required this.games,
-  });
+  const TitleSelectionDialog({super.key, required this.games});
 
   final List<GameInfo> games;
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      content: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ...titleButtons(context),
-            cancelButton(context),
-          ],
-        ),
+    return ChangeNotifierProvider(
+      create: (_) => DialogGamesModel(games),
+      child: Consumer<DialogGamesModel>(
+        builder: (context, model, _) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ...titleButtons(context, model.games),
+                  cancelButton(context),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 
-  Iterable<Widget> titleButtons(BuildContext context) {
-    return games.map((game) {
-      final isFirstGame = games.indexOf(game) == 0;
-      return titleButton(context, game, isFirstGame);
+  Iterable<Widget> titleButtons(BuildContext context, List<GameInfo> games) {
+    return games.asMap().entries.map((entry) {
+      final index = entry.key;
+      final game = entry.value;
+      return titleButton(context, game, index == 0);
     });
   }
 
@@ -38,9 +44,7 @@ class TitleSelectionDialog extends StatelessWidget {
       child: ElevatedButton(
         onPressed: () {
           Navigator.pop(context);
-          BggPage(
-            gameInfo: game,
-          ).open(context);
+          BggPage(gameInfo: game).open(context);
         },
         child: Center(
           child: Text(
@@ -56,16 +60,41 @@ class TitleSelectionDialog extends StatelessWidget {
 
   SizedBox cancelButton(BuildContext context) {
     return SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                side: BorderSide(color: Colors.red, width: 2),
-              ),
-              child: Text('Cancel'),
-            ),
-          );
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        style: ElevatedButton.styleFrom(
+          side: BorderSide(color: Colors.red, width: 2),
+        ),
+        child: Text('Cancel'),
+      ),
+    );
+  }
+}
+
+class DialogGamesModel extends ChangeNotifier {
+  DialogGamesModel(List<GameInfo> games) : _games = List<GameInfo>.from(games);
+
+  final List<GameInfo> _games;
+
+  List<GameInfo> get games => List.unmodifiable(_games);
+
+  void setGames(List<GameInfo> games) {
+    _games
+      ..clear()
+      ..addAll(games);
+    notifyListeners();
+  }
+
+  void addGame(GameInfo game) {
+    _games.add(game);
+    notifyListeners();
+  }
+
+  void removeGame(GameInfo game) {
+    _games.remove(game);
+    notifyListeners();
   }
 }
